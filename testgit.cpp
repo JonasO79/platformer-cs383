b8e26e11a60b7b8ea55883130dbab0bf2390d50c
Jonas Oatman <jonaso@Defaults-MacBook-Air.local>
2024-02-13 09:56:41 -0800

    Initial Commit

---
+++ testgit.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+	for(int j = 0; j > -1; j++){
+	for(int i = 0; i < 100; i++){
+		cout << "0 ";
+	}
+	cout << endl;
+	cout << "     ";
+	}
+	
+}
